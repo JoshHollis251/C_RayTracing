d3f58aedcef644a32319f3abd120b906666d5eb6
Tyler <tyler.wayne2002@hotmail.com>
2024-10-27 20:39:49 -0500

    heh

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,15 @@
 use winit::{
     event::*,
-    event_loop::EventLoopBuilder,
     event_loop::{ControlFlow, EventLoop},
-    keyboard::{KeyCode, PhysicalKey},
     window::WindowBuilder,
 };
-
-#[derive(Debug, Clone, Copy)]
-enum CustomEvent {
-    Timer,
-}
-
+ 
 fn main() {
-    env_logger::init();
-    let event_loop = EventLoop::new();
-    
-    let event_loop: EventLoop<CustomEvent> = EventLoopBuilder::<CustomEvent>::with_user_event()
-        .build()
-        .unwrap();
-
+    env_logger::init(); // Necessary for logging within WGPU
+    let event_loop = EventLoop::new(); // Loop provided by winit for handling window events
     let window = WindowBuilder::new().build(&event_loop).unwrap();
-
-    let event_loop_proxy = event_loop.create_proxy();
-
-    std::thread::spawn(move || loopo {
-        std::thread::sleep(std::time::Duration::from_millis(17));
-        event_loop_proxy.send_event(CustomEvent::Timer).ok();
-    })
-
-    event_loop.run(move | event, elwt | match event {
-        Event::UserEvent(..) => {
-            println!("New Frame");
-        },
-
-        Event::WindowEvent { window_id, event } if window_id == window.id() => match event {
-            WindowEvent::CloseRequested => {
-                println!("Closing Window");
-                elwt.exit();
-            }
-            _ => (),
-        },
-        _ => (),
-    }).expect("Error!");
+ 
+    // Opens the window and starts processing events (although no events are handled yet)
+    event_loop.run(move |event, _, control_flow| {});
 }
+
