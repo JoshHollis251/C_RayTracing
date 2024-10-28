90863f2976a259cb1c16392db1caf575fb9fe626
Tyler <tyler.wayne2002@hotmail.com>
2024-10-27 19:13:15 -0500

    rng

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use rand::Rng;
+
 struct Point {
     x: f64,
     y: f64,
@@ -5,7 +7,12 @@ struct Point {
 
 fn main() {
     println!("Hello World!");
-    println!("I'm a Rustacean!");
+    println!("I'm a Rustacean!"); //cringe
     let p = Point { x: 0.0, y: 0.0 };
-    println!("The point is at ({}, {})", p.x, p.y);
+    for i in 0..10 {
+        //print a random number
+        let x = rand::thread_rng().gen_range(-10.0..10.0);
+        let newPoint = Point { x: rand::thread_rng().gen_range(-10.0..10.0), y: rand::thread_rng().gen_range(-10.0..10.0)};
+        println!("coords: ({}, {})", newPoint.x, newPoint.y);
+    }
 }
