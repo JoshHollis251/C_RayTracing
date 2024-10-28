ad645621be3eb457586b7a1eb5ef745872685110
Tyler <tyler.wayne2002@hotmail.com>
2024-10-27 22:12:57 -0500

    doc implementation

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,40 @@
-//TODO: implement raytracing
-static IS_RAYTRACING: bool = true;
+use winit::application::ApplicationHandler;
+use winit::event::WindowEvent;
+use winit::event_loop::{ActiveEventLoop, ControlFlow, EventLoop};
+use winit::window::{Window, WindowId};
 fn main() {
-    if IS_RAYTRACING {
-        println!("raytracing");
-    } else {
-        println!("not raytracing");
+    #[derive(Default)]
+    struct App {
+        window: Option<Window>,
     }
+    impl ApplicationHandler for App {
+        fn resumed(&mut self, event_loop: &ActiveEventLoop) {
+            println!("Resumed");
+        }
+
+        fn window_event(
+                &mut self,
+                event_loop: &ActiveEventLoop,
+                window_id: WindowId,
+                event: WindowEvent,
+            ) {
+            match event {
+                WindowEvent::CloseRequested => {
+                    println!("Close requested");
+                    event_loop.exit();
+                },
+                WindowEvent::RedrawRequested => {
+                    println!("Redraw requested");
+                }
+                _ => {}
+            }
+        }
+    }
+
+    let event_loop = EventLoop::new().unwrap();
+    event_loop.set_control_flow(ControlFlow::Poll);
+    let mut app = App::default();
+    let _ = event_loop.run_app(&mut app);
+    
+
 }
