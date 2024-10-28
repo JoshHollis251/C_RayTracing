41c3d818379b2c5458271a08264ece6608411fe0
Tyler <tyler.wayne2002@hotmail.com>
2024-10-27 19:06:36 -0500

    point struct

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
-
+struct Point {
+    x: f64,
+    y: f64,
+}
 
 fn main() {
     println!("Hello World!");
     println!("I'm a Rustacean!");
+    let p = Point { x: 0.0, y: 0.0 };
+    println!("The point is at ({}, {})", p.x, p.y);
 }
