55222225b945c73ee4efbc38ea922b348130e763
Tyler <tyler.wayne2002@hotmail.com>
2024-10-27 20:15:39 -0500

    god is dead

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,45 @@
-use rand::Rng;
+use winit::{
+    event::*,
+    event_loop::EventLoopBuilder,
+    event_loop::{ControlFlow, EventLoop},
+    keyboard::{KeyCode, PhysicalKey},
+    window::WindowBuilder,
+};
 
-struct Point {
-    x: f64,
-    y: f64,
+#[derive(Debug, Clone, Copy)]
+enum CustomEvent {
+    Timer,
 }
-
-struct Line {
-    start: Point,
-    end: Point,
-}
-
-static MAGIC_NUMBER: i32 = 30;
 
 fn main() {
-    let unused_var = 42; // removes the unused variable warning on build
+    env_logger::init();
+    let event_loop = EventLoop::new();
     
-    println!("Hello World!");
-    println!("I'm a Rustacean!"); //cringe
-    for i in 0..10 {
-        //print a random number
-        let new_point = Point { x: rand::thread_rng().gen_range(-10.0..10.0), y: rand::thread_rng().gen_range(-10.0..10.0)};
-        println!("coords: ({}, {}) {}", new_point.x, new_point.y, i);
-    }
+    let event_loop: EventLoop<CustomEvent> = EventLoopBuilder::<CustomEvent>::with_user_event()
+        .build()
+        .unwrap();
+
+    let window = WindowBuilder::new().build(&event_loop).unwrap();
+
+    let event_loop_proxy = event_loop.create_proxy();
+
+    std::thread::spawn(move || loopo {
+        std::thread::sleep(std::time::Duration::from_millis(17));
+        event_loop_proxy.send_event(CustomEvent::Timer).ok();
+    })
+
+    event_loop.run(move | event, elwt | match event {
+        Event::UserEvent(..) => {
+            println!("New Frame");
+        },
+
+        Event::WindowEvent { window_id, event } if window_id == window.id() => match event {
+            WindowEvent::CloseRequested => {
+                println!("Closing Window");
+                elwt.exit();
+            }
+            _ => (),
+        },
+        _ => (),
+    }).expect("Error!");
 }
