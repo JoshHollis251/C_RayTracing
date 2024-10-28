e3f49e970564a20ace38958960825a4193101433
Tyler <tyler.wayne2002@hotmail.com>
2024-10-27 19:27:10 -0500

    rng list

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,19 @@ struct Point {
     y: f64,
 }
 
+struct Line {
+    start: Point,
+    end: Point,
+}
+
+static MAGIC_NUMBER: i32 = 30;
+
 fn main() {
     println!("Hello World!");
     println!("I'm a Rustacean!"); //cringe
-    let p = Point { x: 0.0, y: 0.0 };
     for i in 0..10 {
         //print a random number
-        let x = rand::thread_rng().gen_range(-10.0..10.0);
-        let newPoint = Point { x: rand::thread_rng().gen_range(-10.0..10.0), y: rand::thread_rng().gen_range(-10.0..10.0)};
-        println!("coords: ({}, {})", newPoint.x, newPoint.y);
+        let new_point = Point { x: rand::thread_rng().gen_range(-10.0..10.0), y: rand::thread_rng().gen_range(-10.0..10.0)};
+        println!("coords: ({}, {}) {}", new_point.x, new_point.y, i);
     }
 }
