d4db1a691fdffbec2c868a3e7cf9545d2b8b5ceb
Tyler Russell <82682594+Tyler-251@users.noreply.github.com>
2024-10-28 20:12:17 -0500

    Im spongebob!

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,71 @@
 use bevy::prelude::*;
 
+#[derive(Component, PartialEq)] // partialeq allows us to compare the enum
+enum ObjectType {
+    Player,
+    Static
+}
 fn main() {
     let mut app = App::new();
     app.add_plugins(DefaultPlugins)
-    .add_systems(Startup, setup);
+        .add_systems(Startup, setup)//runs setup on Startup
+        .add_systems(Update, control); //runs control on Update
     app.run();
 }
 
-fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
-    commands.spawn(Camera2dBundle::default());
-    commands.spawn(SpriteBundle {
-        texture: asset_server.load("img/spongebob.png"),
-        ..Default::default()
-    });
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) { //sprites rendered bottum-up
+    commands.spawn(Camera2dBundle::default()); //camera is spawned
+    commands.spawn(
+        (SpriteBundle { //spawns pineapple
+            texture: asset_server.load("img/pineapple.png"),
+            sprite: Sprite {
+                custom_size: Some(Vec2::new(200., 300.)),
+                ..default()
+            },
+            transform: Transform::from_translation(Vec3::new(200., 100., 0.)),
+            ..default()
+        },
+        ObjectType::Static,
+    ));
+    commands.spawn( //spawns player
+        (SpriteBundle {
+            texture: asset_server.load("img/spongebob.png"),
+            sprite: Sprite {
+                custom_size: Some(Vec2::new(50., 50.)),
+                ..default()
+            },
+            ..default()
+        },
+        ObjectType::Player,
+    ));
+}
+
+static MOVE_SPEED: f32 = 5.;
+
+fn control(mut query: Query<(&mut ObjectType, &mut Transform)>, //fetches objects with transform and object type
+    input: Res<ButtonInput<KeyCode>>, 
+    asset_server: Res<AssetServer>, //loads audio
+    mut commands: Commands) { //enables spawns
+    for (mut object_type, mut transform) in query.iter_mut() {
+        if *object_type == ObjectType::Player {
+            if input.pressed(KeyCode::KeyW) {
+                transform.translation.y += MOVE_SPEED;
+            }
+            if input.pressed(KeyCode::KeyS) {
+                transform.translation.y -= MOVE_SPEED;
+            }
+            if input.pressed(KeyCode::KeyA) {
+                transform.translation.x -= MOVE_SPEED;
+            }
+            if input.pressed(KeyCode::KeyD) {
+                transform.translation.x += MOVE_SPEED;
+            }
+        }
+    }
+    if input.just_pressed(KeyCode::Space) {
+        commands.spawn( AudioBundle {
+            source: asset_server.load("sound/bob.ogg"), //only use ogg apparently
+            ..default()
+        });
+    }
 }
