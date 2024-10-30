c54b5d528bb75c883fbf4dc9f40d8bc9ca42369a
Tyler Russell <82682594+Tyler-251@users.noreply.github.com>
2024-10-29 21:49:51 -0500

    FEEEEEEEEET

--- src/first_person/spawn_player.rs
+++ src/first_person/spawn_player.rs
@@ -20,8 +20,25 @@ impl Plugin for SpawnPlayerPlugin {
     }
 }
 
-fn init_player(mut commands: Commands, mut focus : ResMut<WindowFocusState>) {
-    commands.spawn((Player, Camera3dBundle::default()));
+fn init_player(
+    mut commands: Commands, 
+    mut focus : ResMut<WindowFocusState>, 
+    mut meshes: ResMut<Assets<Mesh>>, 
+    mut materials: ResMut<Assets<StandardMaterial>>
+) {
+    commands.spawn((
+        Player,
+        SpatialBundle::default()
+    ))
+        .with_children(|parent| {
+            parent.spawn(PbrBundle { // feet
+                mesh: meshes.add(Cuboid::new(0.3, 0.3, 0.5)),
+                material: materials.add(Color::srgb(0.8, 0.7, 0.6)),
+                transform: Transform::from_translation(Vec3::new(0.0, -3.0, 0.0)), // Position the cube at the player's feet
+                ..default()
+            });
+            parent.spawn( Camera3dBundle::default()); // camera
+        });
     focus.focused = true;
 }
 
@@ -31,16 +48,18 @@ static MOVE_SPEED: f32 = 5.0;
 static MAX_PITCH: f32 = std::f32::consts::FRAC_PI_2 - 0.1;
 
 fn control_player_view (
-    mut query: Query<&mut Transform, With<Player>>,
+    mut camera: Query<&mut Transform, (With<Camera>, Without<Player>)>, //fcking ridiculous
+    mut player: Query<&mut Transform, (With<Player>, Without<Camera>)>,
     mut window: Query<&mut Window, With<PrimaryWindow>>,
     mut mouse: EventReader<MouseMotion>,
     mut input: ResMut<ButtonInput<KeyCode>>,
     mut focus: ResMut<WindowFocusState>,
     time: Res<Time>, 
 ) {
-    // Rotate the player
-    let mut transform = query.single_mut(); 
-    let mut pitch = transform.rotation.to_euler(EulerRot::YXZ).1;
+    // Rotate the camera
+    let mut camera_transform = camera.single_mut(); 
+    let mut player_transform = player.single_mut();
+    let mut pitch = camera_transform.rotation.to_euler(EulerRot::YXZ).1;
 
     for motion in mouse.read() {
         let yaw = -motion.delta.x * X_SENSITIVITY;
@@ -48,8 +67,8 @@ fn control_player_view (
 
         pitch = (pitch + delta_pitch).clamp(-MAX_PITCH, MAX_PITCH);
 
-        transform.rotate_y(yaw);
-        transform.rotation = Quat::from_rotation_y(transform.rotation.to_euler(EulerRot::YXZ).0) * Quat::from_rotation_x(pitch);
+        player_transform.rotate_y(yaw);
+        camera_transform.rotation = Quat::from_rotation_y(camera_transform.rotation.to_euler(EulerRot::YXZ).0) * Quat::from_rotation_x(pitch);
     }
 
     // Move the player
@@ -72,21 +91,19 @@ fn control_player_view (
     }
     if direction.length() > 0.0 {
         direction = direction.normalize();
-        
-        // Extract the yaw rotation (rotation around the Y axis)
-        let yaw_rotation = Quat::from_rotation_y(transform.rotation.to_euler(EulerRot::YXZ).0);
-        
-        // Transform the direction by the yaw rotation
+        let yaw_rotation = Quat::from_rotation_y(player_transform.rotation.to_euler(EulerRot::YXZ).0);
         let movement = yaw_rotation * direction;
-        
-        // Normalize the movement direction on the X-Z plane
         let flat_movement = Vec3::new(movement.x, 0.0, movement.z).normalize();
-        
-        // Calculate the final movement vector
         let final_movement = flat_movement * speed * time.delta_seconds();
-        
-        // Apply the movement to the player's transform
-        transform.translation += final_movement;
+        player_transform.translation += final_movement;
+    }
+
+    // Up and Down Movement
+    if input.pressed(KeyCode::Space) {
+        player_transform.translation += Vec3::Y * speed * time.delta_seconds();
+    }
+    if input.pressed(KeyCode::ControlLeft) {
+        player_transform.translation -= Vec3::Y * speed * time.delta_seconds();
     }
 
     // Toggle focus on Escape
